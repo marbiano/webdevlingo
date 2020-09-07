8f0eef9978e7d57437d2816fb056087a749e1704
marbiano <martin@Martins-MacBook-Pro.local>
2020-09-07 18:22:49 -0300

    No need for Sanity client anymore

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,24 +1,21 @@
 import { Fragment } from 'react';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
-import PortableText from '@sanity/block-content-to-react';
 import { fetchAllTerms } from '../lib/api';
 import { Term } from '../lib/types';
 import Layout from '../components/Layout';
-import styles from '../styles/blocks.module.css';
 
 interface PageProps {
   terms: Term[];
 }
 
 const Home: React.FC<PageProps> = ({ terms }) => {
-  console.log(terms);
   return (
     <Layout showcase>
       <div className="container mx-auto px-4 pt-16">
         <ul className="divide-y divide-gray-400">
           {terms.map((term) => (
-            <li className="text-lg">
+            <li key={term._id} className="text-lg">
               <Link href={`/term/${term.slug.current}`}>
                 <a className="block px-4 py-4 hover:bg-gray-200">
                   {term.title}
